6a9aee4b4c78012b615fc77a3eaf84f578cb65aa
phoenixarjun <phoenixarjun@gmail.com>
2025-12-29 16:34:24 +0530

    Implement item retrieval by ending time and views with proper deserialization

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,26 @@
+import { client } from '$services/redis';
+import { itemsByEndingAtKey, itemsKey } from '$services/keys';
+import { deserialize } from './deserialize';
+
 export const itemsByEndingTime = async (
 	order: 'DESC' | 'ASC' = 'DESC',
 	offset = 0,
 	count = 10
-) => {};
+) => {
+	const ids = await client.zRange(
+		itemsByEndingAtKey(),
+		Date.now(),
+		'+inf',
+		{
+			BY: 'SCORE',
+			LIMIT: {
+				offset,
+				count
+			}
+		}
+	);
+	const resutls = await Promise.all(
+		ids.map((id) => client.hGetAll(itemsKey(id)))
+	);
+	return resutls.map((items, i) => deserialize(ids[i], items));
+};
