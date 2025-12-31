dd23b3573b547b718cc2d944226ba616d376a89c
phoenixarjun <phoenixarjun@gmail.com>
2025-12-31 17:41:34 +0530

    Enhance withLock function to support signal for lock expiration and unlock script

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,9 +1,10 @@
 import { randomBytes } from 'crypto';
 import { client } from '../redis/client';
 
-export const withLock = async (key: string, cb: () => any) => {
+export const withLock = async (key: string, cb: (redisClient: Client, signal: any) => any) => {
 	const retryDelayMs = 100;
 	let retries = 20;
+	const timeoutMs = 2000;
 
 	const token = randomBytes(6).toString('hex');
 	const lockKey = `lock:${key}`;
@@ -13,9 +14,14 @@ export const withLock = async (key: string, cb: () => any) => {
 		const acquired = await client.set(lockKey, token, { NX: true, PX: 2000 });
 		if (acquired) {
 			try {
-				return await cb();
+				const signal = { expired: false };
+				setTimeout(() => {
+					signal.expired = true;
+				}, timeoutMs);
+				const proxiedClient = buildClientProxy(timeoutMs);
+				return await cb(proxiedClient, signal);
 			} finally {
-				await client.del(lockKey);
+				await client.unlock(lockKey, token);
 			}
 		}
 		await pause(retryDelayMs);
@@ -23,7 +29,24 @@ export const withLock = async (key: string, cb: () => any) => {
 
 };
 
-const buildClientProxy = () => {};
+
+
+type Client = typeof client;
+const buildClientProxy = (timeoutMs: number) => {
+	const startTime = Date.now();
+	const handler = {
+		get(target: Client, prop: string | symbol){
+			if(Date.now() >= startTime + timeoutMs) {
+				throw new Error('Lock expired');
+			}
+
+			const value = target[prop as keyof Client];
+			return typeof value === 'function' ? value.bind(target) : value;
+		}
+	}
+
+	return new Proxy(client, handler) as Client;
+};
 
 const pause = (duration: number) => {
 	return new Promise((resolve) => {
